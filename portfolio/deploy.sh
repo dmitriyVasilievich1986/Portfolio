a726af21e24b1fd1f384770f8b36a949e053d54a
dmitriy shcherbachenya <dmitriyvasil@gmail.com>
2020-08-16 15:20:37 +0300

    16.08.2020.01

--- portfolio/deploy.sh
+++ portfolio/deploy.sh
@@ -1,7 +1,7 @@
 docker build -t portfolio -f Dockerfile .
 
-heroku container:push web -a aaa-project-portfolio
+heroku container:push web -a portfolio-project-dmitriy
 
-heroku container:release web -a aaa-project-portfolio
+heroku container:release web -a portfolio-project-dmitriy
 
-heroku run python manage.py migrate -a aaa-project-portfolio
+heroku run python manage.py migrate -a portfolio-project-dmitriy
